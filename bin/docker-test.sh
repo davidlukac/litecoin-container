c89007793887d7043f10bed54486d16841079d3f
David Lukac <1215290+davidlukac@users.noreply.github.com>
2021-06-22 18:24:57 +0200

    Testing Grype - causing issues in Travis

--- bin/docker-test.sh
+++ bin/docker-test.sh
@@ -15,5 +15,5 @@ hadolint alpine.dockerfile
 if [ -z ${CI+x} ]; then
   grype -q "sbom:./build/${APP}.sbom.json"
 else
-  docker run --rm -v $(pwd):/opt/app anchore/grype "sbom:/opt/app/build/${APP}.sbom.json"
+  < "build/${APP}.sbom.json" docker run --rm -i anchore/grype -f "$(yq e '.fail-on-severity' .grype.yaml)" -s "$(yq e '.scope' .grype.yaml)" -q
 fi
